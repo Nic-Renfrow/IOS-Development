7bea5fdbcd9b8a6db3c723b4f73575449e841572
Nic <s535876@nwmissouri.edu>
2022-08-23 09:05:01 -0500

    Constants and Print Statements
    Print statements and constants (class work)

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+var greeting = "Hello, playground"
+//print(greeting)
+//print("Him",10,10.5)
+
+//print(" Hello world!" + greeting)
+
+var age = 23
+
+print("My age is \(age)") // String interpolation
+
+//print("My age is" +age) concatenation of different data types is not allowed
+
+print("You are \(age) years old and in another \(age) years, you will be \(age * 2)")
+
+print("Hello all, \rWelcome to Swift programming")
+
+let welcomeMessage : String = "Hello!" //constant
+print(welcomeMessage , "All")
+
+var name:String = "John"
+print(name, 2, "Smith")
+name = "Bob"
+print(name)
+
+print("Welcome to Swift Programming")
+print("Fall 2021")
+print("**********")
+print("Welcome to Swift Programming", terminator: "$")
+print("Fall 2022")
+
+print("The list of numbers are ")
+print(1,2,3,4,5,6)
+print("The new pattern is")
+print(1,2,3,4,5,6, separator: "-")
+
